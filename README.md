0da6ed8862b1b5b98afbe647df93f25f916ef5c1
unknown <andre.systems@gmail.com>
2015-09-14 21:29:53 -0300

    Criacao de repositorio; Configuracao inicial spring-boot

---
+++ README.md
@@ -0,0 +1,1 @@
+# Financial Transfers Scheduling - FTS 
---
+++ src/main/java/br/tur/reservafacil/FTSMain.java
@@ -0,0 +1,12 @@
+package br.tur.reservafacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FTSMain {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(FTSMain.class, args);
+    }
+}
